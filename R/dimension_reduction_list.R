6af8ecacb7e2659414fc4cb3c0e567b1b1ee1edf
Owen Visser <owviss@gmail.com>
2024-10-31 12:56:34 -0400

    Added a better description of the dimension reduction list function.

--- R/dimension_reduction_list.R
+++ R/dimension_reduction_list.R
@@ -1,11 +1,11 @@
 #' creates a list of different levels of dimension reduction.
 #'
 #' @param numeric_matrix numeric matrix.
-#' @inheretParams stats::prcomp
-#' @inheretParams Rtsne::Rtsne
-#' @param ... Additional [Rtsne::Rtsne()] parameters.
+#' @param rank. The maximal number of principle components to be used. See [stats::prcomp()] for more details.
+#' @param dims An integer output for dimensionality of the Rtsne reduction, either 2 or 3. usually 2. See [Rtsne::Rtsne()] for more details.
+#' @param ... Allows input of additional [Rtsne::Rtsne()] parameters.
 #' @return A list of all different dimension reductions recorded. Takes the form of a pca object, pca distance object, pca distance matrix, tsne object, tsne distance object, tsne distance matrix. Each of the following are used in the clustering procedures in `ClustCheck`.
-#' @seealso [stats:prcomp()], [Rtsne::Rtsne()], [stats::dist()]
+#' @seealso [stats::prcomp()], [Rtsne::Rtsne()], [stats::dist()]
 #' @examples
 #' # load base iris dataset from rstudio
 #' data("iris")
@@ -13,15 +13,15 @@
 #'
 #' dimension_reduction_list(x)
 #' @export
-dimension_reduction_list <- function(numeric_matrix, pca_dims = 50, tsne_dims = 3,...){
+dimension_reduction_list <- function(numeric_matrix, rank. = 50, dims = 3,...){
 
-  pca <- stats::prcomp(x = t(numeric_matrix), rank. = pca_dims)$x
+  pca <- stats::prcomp(x = t(numeric_matrix), rank. = rank.)$x
   pca_distance_object <- stats::dist(pca, method = "euclidean")
   pca_distance_matrix <- as.matrix(pca_distance_object)
 
   tsne <- Rtsne::Rtsne(
     X = pca_distance_matrix,
-    dims = tsne_dims,
+    dims = dims,
     perplexity = (ncol(pca_distance_matrix)-1)/3,
     check_duplicates = FALSE,
     pca = FALSE,
